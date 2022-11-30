da6d1a4bfc0859a7fb0b474e1531cba174ca3a5c
Bart Marseille <bart.marseille@gmail.com>
2022-11-30 07:25:58 +0100

    added get_device()

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -6,7 +6,6 @@ TORCH_DTYPES = { 'float32': torch.float32, 'float64': torch.float64}
 
 class MpsDataset(torch.utils.data.Dataset):
     
-
     def __init__(self, X: np.ndarray, Y: np.ndarray):
         self.X = X
         self.Y = Y
@@ -15,6 +14,22 @@ class MpsDataset(torch.utils.data.Dataset):
         return torch.tensor([self.X[idx]], dtype=torch.float32), torch.tensor([self.Y[idx]], dtype=torch.float32)
         
     def __len__(self): return len(self.Y)
+
+
+def get_device():
+    device = 'cpu'
+    if torch.backends.mps.is_available():
+        # this ensures that the current MacOS version is at least 12.3+
+        device=torch.device("mps")
+        # # this ensures that the current current PyTorch installation was built with MPS activated.
+        # print(f'torch mps backend built: {torch.backends.mps.is_built()}')
+    elif torch.backends.cuda.is_available():
+        device = torch.device('cuda')
+    else:
+        device = torch.device('cpu')
+    print('pytorch using device:', device)
+    return device
+
 
 def reset_seed(seed):
     torch.manual_seed(seed)
@@ -25,3 +40,4 @@ def reset_seed(seed):
     torch.backends.cudnn.benchmark = False
 
     np.random.seed(seed)
+
