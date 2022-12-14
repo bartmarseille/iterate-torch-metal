032a7622a096f1fc2b11454a609cc255405e0ebb
Bart Marseille <bart.marseille@gmail.com>
2022-12-14 23:36:31 +0100

    cleaning up code

--- utils/iterated_map_utils.py
+++ utils/iterated_map_utils.py
@@ -2,38 +2,37 @@ import numpy as np
 from typing import Callable
 
 
-def iterate(func: Callable, data: np.array, n: int = 0, **kwargs) -> np.array:
+def iterate(func: Callable, V: np.array, n: int = 0, **kwargs) -> np.array:
 
-    if not isinstance(data, np.ndarray):
-        return iterate(func, np.array(data), n, **kwargs)
+    if not isinstance(V, np.ndarray):
+        return iterate(func, np.array(V), n, **kwargs)
     else:    
-        trajectory = [data]
+        trajectory = [V]
         for t in range(1,n):
             trajectory.append(func(trajectory[-1], **kwargs))
         return np.array(trajectory)
 
-
-def logistic_map(data: np.ndarray, r=3.0) -> np.ndarray:
+def logistic_map(V: np.ndarray, r=3.0) -> np.ndarray:
     """The implementation of a relative population system with:
-    -  variables `data`,
+    -  input variable vector `V`,
     -  r: reproduction parameter
-    that maps to the output `data_hat` of the same type as variable `data`
+    that maps to the output variable vector `V_dot`
     """
     
-    x = data    # relative population size
+    x = V    # relative population size
     
     return r * x * (1 - x)
 
 
-def gauss_map(data: np.ndarray, alpha=6.2, beta=-0.5) -> np.ndarray:
+def gauss_map(V: np.ndarray, alpha=6.2, beta=-0.5) -> np.ndarray:
     """Named after Johann Carl Friedrich Gauss, the function maps the bell shaped Gaussian function similar to the logistic map.
-    -  variables `data`,
+    -  input variable vector V,
     -  parameter alpha
     -  parmeter beta
-    that maps to the output `data_hat` of the same type as variable `data`
+    that maps to the output variable vector `V_dot`
     """
     
-    x = data
+    x = V
 
     return np.exp(-alpha * x**2) + beta
     
