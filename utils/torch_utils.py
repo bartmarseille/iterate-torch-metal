f2755ff8718b21f77ec705801df3b2c1a991242a
Bart Marseille <bart.marseille@gmail.com>
2023-01-17 15:04:59 +0100

    updated reset_seed method

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -1,5 +1,8 @@
 import torch
 import numpy as np
+import random
+import os
+from packaging.version import parse, Version
 
 
 class MpsDataset(torch.utils.data.Dataset):
@@ -20,6 +23,14 @@ class MpsDataset(torch.utils.data.Dataset):
         
     def __len__(self): return len(self.Y)
 
+    def get_data(self):
+        if self.device == torch.device('mps'):
+            # cast data type to float32
+            return torch.tensor(self.X, dtype=torch.float32).to(device=self.device), torch.tensor(self.Y, dtype=torch.float32).to(device=self.device)
+        else:
+            return torch.tensor(self.X), torch.tensor(self.Y)
+
+
 
 def get_device():        
     if not torch.backends.mps.is_built():
@@ -35,13 +46,64 @@ def get_device():
     return device
 
 
-def reset_seed(seed):
-    torch.manual_seed(seed)
-    # torch.backends.mps.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.mps.deterministic = True
-    torch.backends.cudnn.benchmark = False
-
+def reset_seed(seed: int = 33) -> None:
+    """
+    Sets random seed into PyTorch, TensorFlow, Numpy and Random.
+    Args:
+        seed: random seed
+    """
+    try:
+        import torch
+    except ImportError:
+        pass
+    else:
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        torch.backends.mps.deterministic = True
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+    try:
+        import tensorflow as tf
+    except ImportError:
+        pass
+    else:
+        if parse(tf.__version__) >= Version("2.0.0"):
+            tf.random.set_seed(seed)
+        elif parse(tf.__version__) <= Version("1.13.2"):
+            tf.set_random_seed(seed)
+        else:
+            tf.compat.v1.set_random_seed(seed)
+    random.seed(seed)
     np.random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+
+# # For Pytorch
+# def reset_seed(seed: int = 42):
+#     np.random.seed(seed)
+#     random.seed(seed)
+#     torch.manual_seed(seed)
+#     torch.cuda.manual_seed(seed)
+#     # When running on the MPS backend (Apple M1 GPU)
+#     torch.backends.mps.deterministic = True
+#     # When running on the CuDNN backend, two further options must be set
+#     torch.backends.cudnn.deterministic = True
+#     torch.backends.cudnn.benchmark = False
+#     # Set a fixed value for the hash seed
+#     os.environ["PYTHONHASHSEED"] = str(seed)
+#     print(f"Random seed set as {seed}")
+
+# # For TensorFlow
+# def set_seed(seed: int = 42) -> None:
+#   random.seed(seed)
+#   np.random.seed(seed)
+#   tf.random.set_seed(seed)
+#   tf.experimental.numpy.random.seed(seed)
+#   tf.set_random_seed(seed)
+#   # When running on the CuDNN backend, two further options must be set
+#   os.environ['TF_CUDNN_DETERMINISTIC'] = '1'
+#   os.environ['TF_DETERMINISTIC_OPS'] = '1'
+#   # Set a fixed value for the hash seed
+#   os.environ["PYTHONHASHSEED"] = str(seed)
+#   print(f"Random seed set as {seed}")
+
 
